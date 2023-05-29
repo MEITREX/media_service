96bd0d6acfcb39aaa9584ee2745e9bfdbca87417
Paul Bredl <34026908+PaulBredl@users.noreply.github.com>
2023-05-29 16:32:06 +0200

    Remove common code (#8)
    * Add CI-CD Pipeline
    
    * Update gradlew
    
    * Update gradlew again
    
    * Update gradlew again
    
    * Update Docker compose
    
    * clean tests before testing to always run all tests
    
    * add pushing images to gh container registry
    
    * Only start testDb
    
    * Remove testDb
    
    * exclude common code and add dependencies
    
    ---------
    
    Co-authored-by: Keyshal-211 <kkpatel.de@gmail.com>
    Co-authored-by: Valentin Morlock <valentin@morlock.io>
    Co-authored-by: bergdola <116808098+bergdola@users.noreply.github.com>

--- src/test/java/de/unistuttgart/iste/gits/media_service/integration/QueryMediaTest.java
+++ src/test/java/de/unistuttgart/iste/gits/media_service/integration/QueryMediaTest.java
@@ -1,8 +1,19 @@
 package de.unistuttgart.iste.gits.media_service.integration;
 
-import de.unistuttgart.iste.gits.util.GraphQlApiTest;
+
+import de.unistuttgart.iste.gits.common.testutil.GraphQlApiTest;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
 @GraphQlApiTest
-public class QueryMediaTest {
+class QueryMediaTest {
+
+    @Test
+    void testQueryMedia() {
+        // TODO: add tests
+        assertThat(true, is(true));
+    }
 
 }
