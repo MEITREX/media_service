8b3e24d821c93b695af3c2a04c365f7a867bc170
Valentin Morlock <valentin@morlock.io>
2023-07-19 21:51:00 +0000

    change minio external port

--- src/main/java/de/unistuttgart/iste/gits/media_service/config/MinIoConfiguration.java
+++ src/main/java/de/unistuttgart/iste/gits/media_service/config/MinIoConfiguration.java
@@ -33,7 +33,7 @@ public class MinIoConfiguration {
     public MinioClient minioExternalClient() {
         return new MinioClient.Builder()
                 .credentials(accessKey, secretKey)
-                .endpoint(minioExternalUrl, 9000, false)
+                .endpoint(minioExternalUrl, 443, false)
                 .region("eu-central-1")
                 .build();
     }
