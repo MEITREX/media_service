2e4fad960531192eca1556c06b9baafd611918df
Aaron Schmid <56757286+Aaron2000119@users.noreply.github.com>
2025-06-12 17:43:41 +0200

    add test for threadMediaRecordEntity

---
+++ src/test/java/de/unistuttgart/iste/meitrex/media_service/entity/ThreadMediaRecordEntityTest.java
@@ -0,0 +1,51 @@
+package de.unistuttgart.iste.meitrex.media_service.entity;
+
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.MediaRecordEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.QuestionThreadEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.ThreadEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.ThreadMediaRecordReferenceEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.repository.MediaRecordRepository;
+import de.unistuttgart.iste.meitrex.media_service.test_util.MediaRecordRepositoryUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class ThreadMediaRecordEntityTest {
+    private final UUID courseId1 = UUID.randomUUID();
+
+    @Test
+    void testThreadMediaRecordToString(){
+        QuestionThreadEntity questionThread = QuestionThreadEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+        MediaRecordEntity mediaRecord = MediaRecordEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+        int timestamp = 10;
+        int pageNumber = 1;
+        ThreadMediaRecordReferenceEntity threadMediaRecordReference =
+                new ThreadMediaRecordReferenceEntity(questionThread, mediaRecord, timestamp, pageNumber);
+        assertThat(threadMediaRecordReference.toString(), is("{ mediaRecordId: " + mediaRecord.getId()
+                + ", threadId: " + questionThread.getId() + ", timeStampSeconds: 10, pageNumber: 1 }"));
+    }
+
+    @Test
+    void testThreadMediaRecordEquals() {
+        QuestionThreadEntity questionThread = QuestionThreadEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+        MediaRecordEntity mediaRecord = MediaRecordEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+        int timestamp = 10;
+        int pageNumber = 1;
+        ThreadMediaRecordReferenceEntity threadMediaRecordReference =
+                new ThreadMediaRecordReferenceEntity(questionThread, mediaRecord, timestamp, pageNumber);
+        ThreadMediaRecordReferenceEntity threadMediaRecordReference2 =
+                new ThreadMediaRecordReferenceEntity(questionThread, mediaRecord, timestamp, pageNumber);
+        assertThat(threadMediaRecordReference.equals(threadMediaRecordReference2), is(true));
+    }
+}
