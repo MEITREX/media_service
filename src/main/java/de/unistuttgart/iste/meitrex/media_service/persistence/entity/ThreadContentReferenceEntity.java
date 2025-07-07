5c05b29656690eabba9582c62230f1e2379eae85
rost <cornelius.rost@w11k.de>
2025-07-07 16:02:30 +0200

    Add unversioned files

---
+++ src/main/java/de/unistuttgart/iste/meitrex/media_service/persistence/entity/ThreadContentReferenceEntity.java
@@ -0,0 +1,71 @@
+package de.unistuttgart.iste.meitrex.media_service.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.StringJoiner;
+import java.util.UUID;
+
+@Entity(name = "ThreadContentReference")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ThreadContentReferenceEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    UUID contentId;
+
+    @OneToOne
+    ThreadEntity thread;
+
+    @Column
+    private Integer timeStampSeconds;
+
+    @Column
+    private Integer pageNumber;
+
+    public ThreadContentReferenceEntity(ThreadEntity thread, UUID contentId, Integer timeStamp, Integer pageNumber) {
+        this.contentId = contentId;
+        this.thread = thread;
+        this.timeStampSeconds = timeStamp;
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public String toString() {
+        StringJoiner joiner = new StringJoiner(", ", "{ ", " }");
+        if (contentId != null) {
+            joiner.add("contentId: " + contentId);
+        }
+        if (thread != null) {
+            joiner.add("threadId: " + thread.getId());
+        }
+        if (timeStampSeconds != null) {
+            joiner.add("timeStampSeconds: " + timeStampSeconds);
+        }
+        if (pageNumber != null) {
+            joiner.add("pageNumber: " + pageNumber);
+        }
+        return joiner.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ThreadContentReferenceEntity that = (ThreadContentReferenceEntity) o;
+        return Objects.equals(id, that.id) && Objects.equals(contentId, that.contentId) && Objects.equals(thread.getId(), that.thread.getId()) && Objects.equals(timeStampSeconds, that.timeStampSeconds) && Objects.equals(pageNumber, that.pageNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, contentId, thread.getId(), timeStampSeconds, pageNumber);
+    }
+}
---
+++ src/test/java/de/unistuttgart/iste/meitrex/media_service/api_forum/MutationDeleteThreadTest.java
@@ -0,0 +1,102 @@
+package de.unistuttgart.iste.meitrex.media_service.api_forum;
+
+import de.unistuttgart.iste.meitrex.common.testutil.GraphQlApiTest;
+import de.unistuttgart.iste.meitrex.common.testutil.InjectCurrentUserHeader;
+import de.unistuttgart.iste.meitrex.common.user_handling.LoggedInUser;
+import de.unistuttgart.iste.meitrex.generated.dto.QuestionThread;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.ForumEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.PostEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.entity.QuestionThreadEntity;
+import de.unistuttgart.iste.meitrex.media_service.persistence.repository.ForumRepository;
+import de.unistuttgart.iste.meitrex.media_service.persistence.repository.PostRepository;
+import de.unistuttgart.iste.meitrex.media_service.persistence.repository.ThreadRepository;
+import de.unistuttgart.iste.meitrex.media_service.test_util.CourseMembershipUtil;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.test.tester.GraphQlTester;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.UUID;
+
+import static de.unistuttgart.iste.meitrex.common.testutil.TestUsers.userWithMembershipsAndRealmRoles;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+@GraphQlApiTest
+@Transactional
+@ActiveProfiles("test")
+public class MutationDeleteThreadTest {
+    @Autowired
+    private ForumRepository forumRepository;
+
+    private final UUID courseId1 = UUID.randomUUID();
+
+    private final LoggedInUser.CourseMembership courseMembership1 = CourseMembershipUtil.dummyCourseMembershipBuilder(courseId1);
+
+    @InjectCurrentUserHeader
+    private final LoggedInUser currentUser = userWithMembershipsAndRealmRoles(Set.of(LoggedInUser.RealmRole.SUPER_USER), courseMembership1);
+    @Autowired
+    private PostRepository postRepository;
+    @Autowired
+    private ThreadRepository threadRepository;
+
+    @Test
+    void testDeleteThread(final GraphQlTester tester) {
+        ForumEntity forumEntity = ForumEntity.builder()
+                .courseId(courseId1)
+                .threads(new ArrayList<>())
+                .build();
+        forumEntity = forumRepository.save(forumEntity);
+        PostEntity questionEntity = PostEntity.builder()
+                .content("question Content")
+                .authorId(currentUser.getId())
+                .creationTime(OffsetDateTime.now())
+                .build();
+        QuestionThreadEntity threadEntity = QuestionThreadEntity.builder()
+                .forum(forumEntity)
+                .question(questionEntity)
+                .title("Thread Title")
+                .threadContentReferenceEntity(null)
+                .posts(new ArrayList<>())
+                .creatorId(currentUser.getId())
+                .creationTime(OffsetDateTime.now())
+                .numberOfPosts(1)
+                .build();
+        PostEntity postEntity = PostEntity.builder()
+                .content("Post Content")
+                .authorId(currentUser.getId())
+                .creationTime(OffsetDateTime.now())
+                .thread(threadEntity)
+                .build();
+        postEntity = postRepository.save(postEntity);
+        questionEntity.setThread(threadEntity);
+        questionEntity = postRepository.save(questionEntity);
+        threadEntity.getPosts().add(postEntity);
+        threadEntity = threadRepository.save(threadEntity);
+        forumEntity.getThreads().add(threadEntity);
+        forumEntity = forumRepository.save(forumEntity);
+        System.out.println(forumRepository.findAll());
+        System.out.println(threadRepository.findAll());
+        System.out.println(postRepository.findAll());
+        final String query = """
+                mutation {
+                    deleteThread(
+                        threadId: "%s"
+                    ) {
+                        id
+                    }
+                }
+                """.formatted(threadEntity.getId());
+        QuestionThread thread = tester.document(query)
+                .execute()
+                .path("deleteThread").entity(QuestionThread.class).get();
+        assertThat(thread.getId(), is(threadEntity.getId()));
+        assertThat(postRepository.findAll(), hasSize(0));
+        assertThat(threadRepository.findAll(), hasSize(0));
+    }
+}
